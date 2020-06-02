76f12725f37ae58e4becc937a4c391270680a2a1
Oleksandr Rybalov <37306930+sashindionicus@users.noreply.github.com>
2020-06-02 14:42:34 +0300

    Create coffeeMachine.java

---
+++ coffeeMachine.java
@@ -0,0 +1,64 @@
+package machine;
+
+import java.util.Scanner;
+
+public class CoffeeMachine {
+
+
+
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+
+
+
+
+
+
+
+        // --- //
+//        System.out.println("Write how many ml of water the coffee machine has: ");
+//        int waterMl = scanner.nextInt();
+//        System.out.println("Write how many ml of milk the coffee machine has: ");
+//        int milkMl = scanner.nextInt();
+//        System.out.println("Write how many grams of coffee beans the coffee machine has: ");
+//        int beansGrams = scanner.nextInt();
+//        System.out.println("Write how many cups of coffee you will need: ");
+//        int cups = scanner.nextInt();
+//
+//        int minWater = waterMl / 200;
+//        int minMilk = milkMl / 50;
+//        int minBeans = beansGrams / 15;
+//
+//        int one = Math.min(minWater, Math.min(minMilk, minBeans));
+//
+//        if (cups > one) {
+//            System.out.println("No, I can make only " + one + " cup(s) of coffee");
+//        } else if (cups == one) {
+//            System.out.println("Yes, I can make that amount of coffee");
+//        } else {
+//            int coffeeMaker = one - cups;
+//            System.out.printf("Yes, I can make that amount of coffee (and even %d more than that)", coffeeMaker);
+//        }
+        // --- //
+//        System.out.println("Write how many cups of coffee you will need:");
+//        int caps = scanner.nextInt();
+//        int water = 200;
+//        int milk = 50;
+//        int coffeeBeans = 15;
+//        System.out.printf("For %d cups of coffee you will need:\n", caps);
+//        System.out.println(caps * water + " ml of water");
+//        System.out.println(caps * milk + " ml of milk");
+//        System.out.println(caps * coffeeBeans + " g of coffee beans");
+        // --- //
+//        System.out.println("Starting to make a coffee");
+//        System.out.println("Grinding coffee beans");
+//        System.out.println("Boiling water");
+//        System.out.println("Mixing boiled water with crushed coffee beans");
+//        System.out.println("Pouring coffee into the cup");
+//        System.out.println("Pouring some milk into the cup");
+//        System.out.println("Coffee is ready!");
+    }
+}
